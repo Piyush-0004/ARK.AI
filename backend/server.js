5b5b32af640e2e283a6b2f4d2278a0f1bf94a39f
Piyush Pandey <ppiyushkumar274@gmail.com>
2025-07-23 13:29:33 +0530

    fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ const app = express();
 const PORT = 8080;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: 'https://ark-ai-l8in.onrender.com'}));    // allow frontend url to connect backend 
+app.options('*', cors());   // handle preflight requests
 
 app.use("/api", chatRoutes);
 
